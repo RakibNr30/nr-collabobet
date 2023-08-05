387e0168a0a9d0ab813336532d55bc8d4de25d5e
Abdur Rakib <rakib.cse14th@gmail.com>
2023-08-06 03:32:39 +0600

    most of the functionality done.

--- app/Http/Controllers/Portal/DashboardController.php
+++ app/Http/Controllers/Portal/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Portal;
 
+use App\Constants\TransactionStatus;
+use App\Constants\TransactionType;
+use App\Constants\UserType;
+use App\Helpers\AuthUser;
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use App\Models\User;
 
 class DashboardController extends Controller
@@ -11,6 +16,15 @@ class DashboardController extends Controller
     {
         $authUser = User::query()->find(auth()->user()->id);
 
-        return view('portal.dashboard.index', $authUser);
+        $statistics = new \stdClass();
+
+        if (AuthUser::isAdmin()) {
+            $statistics->total_user = User::query()->where('user_type', UserType::USER)->count();
+            $statistics->total_transaction = Transaction::query()->where('type', TransactionType::OUT)->count();
+            $statistics->total_accepted_transaction = Transaction::query()->where('type', TransactionType::OUT)->where('status', TransactionStatus::ACCEPTED)->count();
+            $statistics->total_accepted_transaction_amount = Transaction::query()->where('type', TransactionType::OUT)->where('status', TransactionStatus::ACCEPTED)->sum('amount');
+        }
+
+        return view('portal.dashboard.index', compact('authUser', 'statistics'));
     }
 }
--- resources/views/portal/dashboard/index.blade.php
+++ resources/views/portal/dashboard/index.blade.php
@@ -118,6 +118,105 @@
 
     @endif
 
+    @if(\App\Helpers\AuthUser::isAdmin())
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <a href="{{ route('portal.user.index') }}">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="numbers">
+                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Users</p>
+                                            <h5 class="font-weight-bolder mb-0">
+                                                {{ $statistics->total_user }}
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                            <i class="fas fa-user text-lg opacity-10" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <a href="{{ route('portal.transaction.index') }}">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="numbers">
+                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Transactions</p>
+                                            <h5 class="font-weight-bolder mb-0">
+                                                {{ $statistics->total_transaction }}
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                            <i class="fas fa-coins text-lg opacity-10" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <a href="{{ route('portal.transaction.index') }}">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="numbers">
+                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Verified Transactions</p>
+                                            <h5 class="font-weight-bolder mb-0">
+                                                {{ $statistics->total_accepted_transaction }}
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                            <i class="fas fa-credit-card text-lg opacity-10" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <a href="{{ route('portal.transaction.index') }}">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="numbers">
+                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Verified Amount</p>
+                                            <h5 class="font-weight-bolder mb-0">
+                                                {{ $statistics->total_accepted_transaction_amount }}
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                            <i class="fas fa-money-check-alt text-lg opacity-10" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endif
+
 
 @endsection
 @push('dashboard')
